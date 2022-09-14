e712331a32a1b26abfb9ddf55ae5318303992c45
sseezov <sseezov@mail.ru>
2022-09-14 11:52:31 +0700

    fix:рефакторинг для тестов1

--- bin/brain-games.js
+++ bin/brain-games.js
@@ -1,10 +1,8 @@
 import greet from '../src/cli.js';
-import brainPrime from './games/brain-prime.js';
 
 function brainGames() {
   console.log('Welcome to the Brain Games!');
-  const name = greet();
-  brainPrime(name, 3);
+  greet();
 }
 
 brainGames();
