fe77c657857d991f169a972151bed17af444a1d4
sseezov <sseezov@mail.ru>
2022-10-19 11:59:14 +0700

    fix:исправил Hi на Hello, перенес приветствие в функцию Greet

--- bin/brain-games.js
+++ bin/brain-games.js
@@ -1,7 +1,6 @@
 import greet from '../src/cli.js';
 
 function brainGames() {
-  console.log('Welcome to the Brain Games!');
   greet();
 }
 
--- src/cli.js
+++ src/cli.js
@@ -1,7 +1,8 @@
 import { question } from 'readline-sync';
 
 export default function greet() {
+  console.log('Welcome to the Brain Games!');
   const userName = question('May I have your name? ');
-  console.log(`Hi, ${userName}!`);
+  console.log(`Hello, ${userName}!`);
   return userName;
 }
