f7bbf665ea1b9614199add07287e93b416205f6d
sseezov <sseezov@mail.ru>
2023-01-10 13:38:52 +0700

    fix: minor fix

--- bin/brain-games.js
+++ bin/brain-games.js
@@ -1,3 +1,7 @@
 #!/usr/bin/env node
 
+import { question } from 'readline-sync';
+
 console.log('Welcome to the Brain Games!');
+const userName = question('May I have your name? ');
+console.log(`Hello, ${userName}!`);
